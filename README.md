ae35e36816dd244c1ba52a3fdd66297d04f4789a
t32k <ijok.ijok@gmail.com>
2018-11-12 20:26:21 +0900

    Update background.js

--- README.md
+++ README.md
@@ -2,13 +2,10 @@
 
 > This is a chrome extension to disable JavaScript execution with one click.
 
-
 ## How to Use
 
 [![capture](/artworks/youtube.png)](https://www.youtube.com/watch?v=DO2NLd-PUJw)
 
-
-
 ## Download
 
-+ [Chrome Web Store - jsoff](https://chrome.google.com/webstore/detail/jsoff/kjhbibcocglfnpllfodaiabanmmegomm)
+- [Chrome Web Store - jsoff](https://chrome.google.com/webstore/detail/jsoff/kjhbibcocglfnpllfodaiabanmmegomm)
--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,37 +1,27 @@
-var isOff = true;
+let isOff = true;
 
-chrome.browserAction.onClicked.addListener(function (tab) {
-  if (isOff) {
-    chrome.contentSettings.javascript.set({
-      'primaryPattern': '<all_urls>',
-      'setting': 'block',
-      'scope': 'regular'
-    });
-    chrome.browserAction.setIcon({
-      path: {
-        '19': 'images/icon-38-off.png',
-        '38': 'images/icon-76-off.png'
-      }
-    });
-    chrome.browserAction.setTitle({
-      title: 'JavaScript: OFF'
-    });
-    isOff = false;
-  } else {
-    chrome.contentSettings.javascript.set({
-      'primaryPattern': '<all_urls>',
-      'setting': 'allow',
-      'scope': 'regular'
-    });
-    chrome.browserAction.setIcon({
-      path: {
-        '19': 'images/icon-38.png',
-        '38': 'images/icon-76.png'
-      }
-    });
-    isOff = true;
-  }
-  chrome.tabs.update(tab.id, {
-    url: tab.url
+const setContentsJS = flag => {
+  // Set contents settings
+  chrome.contentSettings.javascript.set({
+    primaryPattern: "<all_urls>",
+    setting: flag ? "block" : "allow",
+    scope: "regular"
   });
-});	
+  // Set contents settings
+  chrome.browserAction.setIcon({
+    path: {
+      "19": `images/icon-38${flag ? "-off" : ""}.png`,
+      "38": `images/icon-76${flag ? "-off" : ""}.png`
+    }
+  });
+  // Set title
+  chrome.browserAction.setTitle({
+    title: `JavaScript: ${flag ? "OFF" : "ON"}`
+  });
+  isOff = !flag;
+};
+
+chrome.browserAction.onClicked.addListener(tab => {
+  setContentsJS(isOff);
+  chrome.tabs.update(tab.id, { url: tab.url });
+});
